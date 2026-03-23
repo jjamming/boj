d817057ddd793b83bd89b7c1808ec4410634a58d
유재민 <162856159+jjamming@users.noreply.github.com>
2026-03-24 00:08:19 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,16 +1,13 @@
 function solution(clothes) {
-    var answer = 1;
+    let answer = 1;
     const map = new Map();
-    
     for(const [name, type] of clothes){
-        map.set(type, map.get(type) + 1 || 1);
+        map.set(type, (map.get(type) || 1) + 1);
     }
     
-    for(const [type, count] of map){
-        
-        answer *= count+1
+    for(const [type, count] of [...map]){
+        answer = answer * count;
     }
-    
     
     return answer - 1;
 }
