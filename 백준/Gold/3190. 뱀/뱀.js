c5fcda9d00210269c1cbad3c59265381612f6b0b
유재민 <162856159+jjamming@users.noreply.github.com>
2026-04-04 00:10:52 +0900

    [Gold IV] Title: 뱀, Time: 204 ms, Memory: 12776 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.js
+++ 백준/Gold/3190. 뱀/뱀.js
@@ -1,70 +1,67 @@
-// 뱀 #3190, 삼성 기출문제집
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-let n = Number(input[0]);
-let k = Number(input[1]);
-let l = Number(input[2 + k]);
+let line = 0;
+const N = Number(input[line++]);
+const K = Number(input[line++]);
 
-let matrix = [];
-let turn = [];
+const matrix = Array.from({ length: N }, () => Array(N).fill(0));
 
-for (let i = 0; i <= n; i++) {
-  matrix[i] = [];
-  for (let j = 0; j <= n; j++) {
-    matrix[i][j] = 0;
-  }
+for (let k = 0; k < K; k++) {
+  const [i, j] = input[line++].split(" ").map(Number);
+  matrix[i-1][j-1] = 2; // 사과 있는 칸: 2
 }
 
-for (let i = 2; i < 2 + k; i++) {
-  let [x, y] = input[i].split(" ").map(Number);
-  matrix[x][y] = 1;
+const L = Number(input[line++]);
+const dChange = [];
+
+for (let i = 0; i < L; i++) {
+  const [X, C] = input[line++].split(" ");
+  dChange.push([Number(X), C.trim()]);
 }
 
-for (let i = 3 + k; i < 3 + k + l; i++) {
-  let [x, c] = input[i].split(" ");
-  turn[x] = c;
-}
+const dx = [-1, 0, 1, 0];
+const dy = [0, 1, 0, -1];
 
+matrix[0][0] = 1; // 뱀 있는 칸: 1
 let time = 0;
-let dir = 0; // 0:E, 1:S, 2:W, 3:N
-const dx = [0, 1, 0, -1];
-const dy = [1, 0, -1, 0];
-
-const snake = [[1, 1]];
-let headIdx = 0;
-let sx = 1;
-let sy = 1;
-matrix[1][1] = 2;
+let [cx, cy, cd] = [0, 0, 1]; // 최초 머리, 방향
+const snake = [[0, 0]];
 
 while (true) {
   time++;
 
-  const nx = sx + dx[dir];
-  const ny = sy + dy[dir];
+  // 먼저 뱀은 몸길이를 늘려 머리를 다음칸에 위치시킨다.
+  const [nx, ny] = [cx + dx[cd], cy + dy[cd]];
+  if (nx < 0 || ny < 0 || nx >= N || ny >= N) break; // 벽에 부딪힘
+  if (matrix[nx][ny] === 1) break; // 자기 몸에 부딪힘
 
-  if (nx < 1 || ny < 1 || nx > n || ny > n || matrix[nx][ny] === 2) {
-    // 머리가 간 곳이 벽 || 뱀의 몸이라면
-    console.log(time);
-    break;
-  }
-
-  if (matrix[nx][ny] !== 1) {
-    // 사과가 아니라면 꼬리 한칸 줄이기
-    const [tx, ty] = snake[headIdx++];
-    matrix[tx][ty] = 0;
-  }
-
-  // 뱀 머리 이동
   snake.push([nx, ny]);
-  matrix[nx][ny] = 2;
-  sx = nx;
-  sy = ny;
 
-  if (turn[time] === "D") {
-    // 시계방향으로 변경
-    dir = (dir + 1) % 4;
-  } else if (turn[time] === "L") {
-    // 반시계방향으로 변경
-    dir = (dir + 3) % 4;
+  // 만약 이동한 칸에 사과가 있다면, 그 칸에 있던 사과가 없어지고 꼬리는 움직이지 않는다.
+  if (matrix[nx][ny] === 2) {
+    matrix[nx][ny] = 1;
+  } else {
+    // 만약 이동한 칸에 사과가 없다면, 몸길이를 줄여서 꼬리가 위치한 칸을 비워준다. 즉, 몸길이는 변하지 않는다.
+    const [tx, ty] = snake.shift();
+    matrix[tx][ty] = 0;
+    matrix[nx][ny] = 1;
   }
+
+  for (const [t, nd] of dChange) {
+    if (t > time) break; // early return
+    if (time === t) {
+      if (nd === "L") {
+        // 반시계 회전
+        cd = (cd + 3) % 4;
+      } else {
+        // nd === "D"
+        // 시계 회전
+        cd = (cd + 1) % 4;
+      }
+    }
+  }
+
+  [cx, cy] = [nx, ny];
 }
+
+console.log(time);
